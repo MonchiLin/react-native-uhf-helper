c3394c1a149375848cd53b2cda1319e365ea38ab
muxi2015 <lin912305043@gmail.com>
2019-03-21 19:13:48 +0800

    增加参数类型定义

--- lib/js/index.ts
+++ lib/js/index.ts
@@ -3,10 +3,16 @@ import {NativeModules, NativeEventEmitter} from "react-native";
 const {UHF} = NativeModules;
 const eventEmitter = new NativeEventEmitter(UHF);
 
+type UHFOptions = {
+    interval?: number,
+}
+
+const initWrapped = (options: UHFOptions = {}) => UHF.initUHF(options);
+
 export default {
-    readerStart: UHF.readerStart as (...args: any) => void,
+    readerStart: UHF.readerStart as () => void,
     readerStop: UHF.readerStop as () => void,
-    initUHF: UHF.initUHF as () => void,
+    initUHF: initWrapped,
     addListener: (listener: (...args: any) => any) =>
         eventEmitter.addListener("reader", listener)
 };
