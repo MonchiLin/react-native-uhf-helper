2ecec36a0c91a100fa3e66a283a76c65f9b0b4ce
muxi2015 <lin912305043@gmail.com>
2019-03-22 14:19:46 +0800

    完善 example

--- example/index.tsx
+++ example/index.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import UHF from "react-native-uhf-helper";
-import {View, Button, Text} from "react-native";
+import {View, Button, Text, FlatList} from "react-native";
 
 // 链接 android log-cat, 标签选择 kiki 即可
 export default class RNUHFTest extends React.Component {
     state = {
-        active: {}
+        tags: []
     };
 
     componentWillMount(): void {
         UHF.initUHF();
+        UHF.addListener((res: number[]) => {
+            if (res.length === 0) {
+                return;
+            }
+            this.setState({tags: [...this.state.tags, res[0]]});
+        });
     }
 
     render(): React.ReactNode {
         return (
             <View>
-                <Button title={"start"} onPress={e => UHF.readerStart()}/>
-                <Button color={"#b2ffaa"} title={"stop"} onPress={e => UHF.readerStop()}/>
+                <Button title={"start"}
+                        onPress={e => UHF.readerStart()}/>
+                <Button color={"#b2ffaa"} title={"stop"}
+                        onPress={e => UHF.readerStop()}/>
+                <FlatList
+                    data={this.state.tags}
+                    renderItem={(tag) => <Text key={tag.index}>{tag.item}</Text>}/>
             </View>
         );
     }
